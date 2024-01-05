0c2108f6ea7f8b34d0af28d8994bad2885676394
Radopopo <radomir.poposki@gmail.com>
2024-01-05 10:06:18 +0100

    latest changes

--- src/config.py
+++ src/config.py
@@ -1,15 +1,19 @@
+import os
+
 class BaseConfig:
     TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATION = False
 
 
 class DevelopmentConfig(BaseConfig):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 
 class TestingConfig(BaseConfig):
     TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_TEST_URL')
+    SECRET_KEY = 'my_precious'
 
 
 class ProductionConfig(BaseConfig):
-    pass
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
